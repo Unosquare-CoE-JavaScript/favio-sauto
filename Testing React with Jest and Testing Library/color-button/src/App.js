3d636ccecda6cb68484f71ddc4e267f5bc8f2e39
Favio Sauto <favio.sauto@unosquare.com>
2022-07-04 16:04:46 -0400

    COE: jest course fixed

--- Testing React with Jest and Testing Library/color-button/src/App.js
+++ Testing React with Jest and Testing Library/color-button/src/App.js
@@ -1,15 +1,35 @@
-import Container from "react-bootstrap/Container";
-import OrderEntry from "./pages/entry/OrderEntry";
-import { OrderDetailsProvider } from "./contexts/OrderDetails";
-import SummaryForm from "./pages/summary/SummaryForm";
+import { useState } from "react";
+
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
+  const [disabled, setDisabled] = useState(false);
+
+  const newButtonColor =
+    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
+
   return (
-    <Container>
-      <OrderDetailsProvider>
-        <OrderEntry />
-      </OrderDetailsProvider>
-    </Container>
+    <div>
+      <button
+        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
+        Change to {replaceCamelWithSpaces(newButtonColor)}
+      </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
+    </div>
   );
 }
 
