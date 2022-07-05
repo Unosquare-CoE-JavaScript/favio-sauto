37caf8812c816769016ebc57e60816d9ebb2b02c
Favio Sauto <favio.sauto@unosquare.com>
2022-07-05 17:27:43 -0400

    Finished the Go for Javascript Developers

---
+++ Go for Javascript Developers/code/01_introduction/exercise.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// // Part One
+// func main() {
+// fmt.Println("Hello World")
+// }
+
+// // Part Two
+func main() {
+fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t", "Favio", "Santa Cruz", 24, true)
+}
